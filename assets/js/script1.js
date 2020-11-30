4c820dade10b39a9cf44ce452190fe1c055b39b3
Sh Maruf <74917669+shmaruf58@users.noreply.github.com>
2020-11-30 16:55:36 +0300

    Update script1.js

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
   document.querySelector(".preloader").classList.add("opacity-0");
   setTimeout(function () {
     document.querySelector(".preloader").style.display = "none";
-  }, 1000);
+  }, 200);
 });
 
 // iTyped
